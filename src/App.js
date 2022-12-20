2ef7de14d7fbfaccccc83bf3149f588476b80244
ifeanyi305 <105528809+ifeanyi305@users.noreply.github.com>
2022-12-20 03:11:23 +0100

    Modify app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
 import ProfilePage from './components/Profile';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
+import MissionsPage from './components/Missions';
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route element={<Navbar />} exact path="/">
+          <Route element={<MissionsPage />} path="/missions" />
           <Route element={<ProfilePage />} path="/profile" />
         </Route>
       </Routes>
